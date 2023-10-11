52ec3fc75b0517c824186c914e1bf11778b23d2d
Emmanuel Omole <emmanueliyare7@gmail.com>
2023-10-11 12:40:46 +0100

    loading state added to each category component

--- src/components/Categories/MensClothing/MensClothing.js
+++ src/components/Categories/MensClothing/MensClothing.js
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import './MensClothing.css';
 import Navbar from "../../Navbar/navbar";
+import loader from "../../../assets/Spinner-1s-200px.svg";
 
 const MensClothing = ({ updatecart, cartItems  }) =>{
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function fetchProducts() {
+    setIsLoading(true);
     fetch("https://fakestoreapi.com/products/category/men's clothing")
       .then(res => res.json())
-      .then(productsData => setProducts(productsData));
+      .then(productsData => {
+        setProducts(productsData);
+        setIsLoading(false);
+      });
   }
 
   useEffect(()=>{
@@ -22,14 +28,15 @@ const MensClothing = ({ updatecart, cartItems  }) =>{
         <div>
           <Navbar noOfItemsIncart={cartItems.length} cartItems={cartItems} />
           <h2>Men's Clothing</h2>
-            {products.map((product)=>{
-                return <div className="product" key={product.id}>
-                <img src={product.image} alt="product.name" />
-                <p><b>{product.title}</b></p>
-                        <p>${product.price}</p>
-						<button style={btnStyle} onClick={()=>updatecart(product)}>Add to Cart</button>
-                        </div>
-            })}
+            {isLoading ? <img src={loader} alt="" height="100px" width="100px" />
+                        : products.map((product)=>{
+                        return <div className="product" key={product.id}>
+                        <img src={product.image} alt="product.name" />
+                        <p><b>{product.title}</b></p>
+                                <p>${product.price}</p>
+                    <button style={btnStyle} onClick={()=>updatecart(product)}>Add to Cart</button>
+                                </div>
+            }) }
         </div>
     )
 }
--- src/components/Categories/WomensClothing/WomensClothing.jsx
+++ src/components/Categories/WomensClothing/WomensClothing.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 import './WomensClothing.css';
 import Navbar from "../../Navbar/navbar";
+import loader from "../../../assets/Spinner-1s-200px.svg";
+
 
 const WomensClothing = ({ updatecart, cartItems }) =>{
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function fetchProducts() {
+    setIsLoading(true);
     fetch("https://fakestoreapi.com/products/category/women's clothing")
       .then(res => res.json())
-      .then(productsData => setProducts(productsData));
+      .then(productsData => {
+        setProducts(productsData);
+        setIsLoading(false);
+      });
   }
 
   useEffect(()=>{
@@ -22,14 +29,15 @@ const WomensClothing = ({ updatecart, cartItems }) =>{
         <div>
           <Navbar noOfItemsIncart={cartItems.length} cartItems={cartItems} />
           <h2>Women's Clothing</h2>
-            {products.map((product)=>{
-                return <div className="product" key={product.id}>
-                <img src={product.image} alt="product.name" />
-                <p><b>{product.title}</b></p>
-                        <p>${product.price}</p>
-						<button style={btnStyle} onClick={()=>updatecart(product)}>Add to Cart</button>
-                        </div>
-            })}
+          {isLoading ? <img src={loader} alt="" height="100px" width="100px" />
+                        : products.map((product)=>{
+                        return <div className="product" key={product.id}>
+                        <img src={product.image} alt="product.name" />
+                        <p><b>{product.title}</b></p>
+                                <p>${product.price}</p>
+                    <button style={btnStyle} onClick={()=>updatecart(product)}>Add to Cart</button>
+                                </div>
+            }) }
         </div>
     )
 }

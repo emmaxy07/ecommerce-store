e50559720df1df713e8313ddbc1682173540fd9a
Emmanuel Omole <emmanueliyare7@gmail.com>
2023-11-20 13:42:26 +0100

    abstraction to homae page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar/navbar';
-import ProductList from './components/Product-list/product-list';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import Cart from './components/Cart/cart';
+import { Route, Routes } from 'react-router-dom';
 import CartPage from './components/Cart/CartPage';
 import MensClothing from './components/Categories/MensClothing/MensClothing';
 import WomensClothing from "./components/Categories/WomensClothing/WomensClothing";
@@ -11,45 +8,20 @@ import Electronics from './components/Categories/Electronics/Electronics';
 import Jewelry from './components/Categories/Jewelry/Jewelry';
 import Login from './components/Login/Login';
 import { AppProvider } from './AppProviderContext';
-import { useDispatch, useSelector } from 'react-redux';
-import { setLogin } from './components/ShoppingCart/shoppingCartSlice';
-import AppLogout from './components/AppLogout';
-import LogoutWarningModal from './components/Modal/LogoutWarningModal';
+import { useSelector } from 'react-redux';
 import SingleProduct from './components/SingleProduct/SingleProduct';
 import CheckoutPage from './components/CheckoutPage/CheckoutPage';
 import HomePage from './components/HomePage/HomePage';
 
 
-// const getTokenFromLocalStorage = () => {
-//   return localStorage.getItem('token');
-// };
-
 const App = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [products, setProducts] = useState([]);
-  // const [cartItems, setCartItems] =useState([]);
 	const [showCart, setShowCart] = useState(false);
-  // const [username, setUsername] = useState("");
-
 
   const {
     isLoggedIn,
     cartItems,
     username,
   } = useSelector(store => store.shoppingCart);
-
-  // const onRemove = (product) => {
-  //   const exist = cartItems.find((x) => x.id === product.id);
-  //   if (exist.qty === 1) {
-  //     setCartItems(cartItems.filter((x) => x.id !== product.id));
-  //   } else {
-  //     setCartItems(
-  //       cartItems.map((x) =>
-  //         x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
-  //       )
-  //     );
-  //   }
-  // };
 
   const viewCart = () => cartItems;
 
@@ -67,18 +39,6 @@ const App = () => {
        {isLoggedIn && <Route
         path='/home'
         element={
-//           <>
-//           <AppLogout>
-//         <Navbar setShowCart={setShowCart} viewCart={viewCart} firstChar={firstChar}
-//                   firstCharAfterSpace={firstCharAfterSpace} userImage={userImage} />
-//                   {online ? ""
-//  : <div style={{backgroundColor: "red", color: "white", fontSize: "20px", height: "50px", paddingTop: "25px"}}>You are offline</div>}
-//  {welcomeBack && <div className='welcome-back'>Welcome back! You are online</div>}
-//       <Cart showCart={showCart} />
-//       {/* <LogoutWarningModal /> */}
-//       <ProductList />
-//       </AppLogout>
-//       </>
 <HomePage setShowCart={setShowCart} viewCart={viewCart} firstChar={firstChar} firstCharAfterSpace={firstCharAfterSpace} showCart={showCart} />
         }
       />}
